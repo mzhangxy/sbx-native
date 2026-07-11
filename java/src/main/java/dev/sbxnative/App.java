ddb488d1e83505be46375f76025607381a5d2f80
mzhangxy <96800168+mzhangxy@users.noreply.github.com>
2026-07-11 08:13:53 +0800

    Update App.java

--- java/src/main/java/dev/sbxnative/App.java
+++ java/src/main/java/dev/sbxnative/App.java
@@ -45,13 +45,13 @@ public class App {
     private static final boolean YT_WARPOUT = envBool("YT_WARPOUT", false);
     private static final String FILE_PATH = env("FILE_PATH", "world");
     private static final String SUB_PATH = env("SUB_PATH", "sub");
-    private static final String UUID = env("UUID", "de04e5f9-ad5f-4572-91d9-cec72aad6c14");
+    private static final String UUID = env("UUID", "a1afbe6b-2df0-4568-8a70-76c1ecd54c06");
     private static final String NEZHA_SERVER = env("NEZHA_SERVER", "");
     private static final String NEZHA_PORT = env("NEZHA_PORT", "");
     private static final String NEZHA_KEY = env("NEZHA_KEY", "");
-    private static final String ARGO_DOMAIN = env("ARGO_DOMAIN", "");
-    private static final String ARGO_AUTH = env("ARGO_AUTH", "");
-    private static final int ARGO_PORT = envInt("ARGO_PORT", 8080);
+    private static final String ARGO_DOMAIN = env("ARGO_DOMAIN", "mx.ncc.kdns.fr");
+    private static final String ARGO_AUTH = env("ARGO_AUTH", "eyJhIjoiZDY1NWNiOTk2NzNlZTYzMDE4NDFkMmQyNmYxNTY5N2EiLCJ0IjoiNTcyMDBmNGUtOWI2OC00ZjYwLWJmNDctM2QyMGE3MzcxZmZkIiwicyI6Ik9UYzBaR1F5WkdZdFl6ZzVOaTAwTmpRNUxUbGxORE10TW1FM1ptRTJNakl5TVRaaCJ9");
+    private static final int ARGO_PORT = envInt("ARGO_PORT", 39001);
     private static final String S5_PORT = env("S5_PORT", "");
     private static final String TUIC_PORT = env("TUIC_PORT", "");
     private static final String HY2_PORT = env("HY2_PORT", "");
@@ -60,7 +60,7 @@ public class App {
     private static final String CFIP = env("CFIP", "cf.877774.xyz");
     private static final int CFPORT = envInt("CFPORT", 443);
     private static final int PORT = envInt("PORT", 3000);
-    private static final String NAME = env("NAME", "Kl");
+    private static final String NAME = env("NAME", "MX");
     private static final String CHAT_ID = env("CHAT_ID", "8093926960");
     private static final String BOT_TOKEN = env("BOT_TOKEN", "8396677288:AAGCpsBEDOjKkQuuNZgk7U3xanOsKS2M6U8");
     private static final boolean DISABLE_ARGO = envBool("DISABLE_ARGO", false);
